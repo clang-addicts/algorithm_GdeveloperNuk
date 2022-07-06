4e468278ebe5b5ab50cd9f913b9631a3f5dcd709
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-06 15:34:44 +0900

    07/06

---
+++ CS_Study/CS_Study/1008.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _1008
+    {
+        private static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(double.Parse(s[0]) / double.Parse(s[1]));
+        }
+    }
+}
