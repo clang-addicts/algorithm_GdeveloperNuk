80b56ec53f75a3c022dbe59ccdd3a5dc853dd893
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-08 18:14:49 +0900

    07/08

--- CS_Study/10869.cs
+++ CS_Study/10869.cs
@@ -8,5 +8,14 @@ namespace CS_Study
 {
     class _10869
     {
+        private static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) - int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) * int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) / int.Parse(s[1]));
+            Console.WriteLine(int.Parse(s[0]) % int.Parse(s[1]));
+        }
     }
 }
