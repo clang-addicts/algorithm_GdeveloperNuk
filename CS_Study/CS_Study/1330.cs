9a40dc9a46c16bceffecaca00d737feb667175cd
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-11 23:03:52 +0900

    07/11

---
+++ CS_Study/CS_Study/1330.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _1330
+    {
+        private static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            if(int.Parse(s[0]) > int.Parse(s[1]))
+            {
+                Console.WriteLine(">");
+            }
+            else if (int.Parse(s[0]) < int.Parse(s[1]))
+            {
+                Console.WriteLine("<");
+            }
+            if (int.Parse(s[0]) == int.Parse(s[1]))
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
