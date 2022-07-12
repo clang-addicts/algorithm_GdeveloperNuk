3faefeae492e8b1dcd023561b98ad643f9de48d9
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-12 21:44:21 +0900

    07/12
    9498

---
+++ CS_Study/CS_Study/9498.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _9498
+    {
+        private static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            switch (a / 10)
+            {
+                case 10:
+                case 9:
+                    Console.WriteLine("A");
+                    break;
+                case 8:
+                    Console.WriteLine("B");
+                    break;
+                case 7:
+                    Console.WriteLine("C");
+                    break;
+                case 6:
+                    Console.WriteLine("D");
+                    break;
+                default:
+                    Console.WriteLine("F");
+                    break;
+            }
+        }
+    }
+}
