80dd080c34c5fc06e154d4f044127ebda074750a
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-13 20:38:30 +0900

    07/13

---
+++ CS_Study/CS_Study/2753.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _2753
+    {
+        private static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int a = int.Parse(s);
+            if((a % 4 == 0 && a % 100 != 0) || a % 400 == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
