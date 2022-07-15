0bc901a1f24a5e61dbefd924b678bc897666af80
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-15 10:10:13 +0900

    07/15

---
+++ CS_Study/CS_Study/14681.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _14681
+    {
+        private static void Main(string[] args)
+        {
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if(x > 0)
+            {
+                if(y > 0)
+                {
+                    Console.Write(1);
+                }
+                else if(y < 0)
+                {
+                    Console.Write(4);
+                }
+            }
+            else if(x < 0)
+            {
+                if (y > 0)
+                {
+                    Console.Write(2);
+                }
+                else if (y < 0)
+                {
+                    Console.Write(3);
+                }
+            }
+        }
+    }
+}
