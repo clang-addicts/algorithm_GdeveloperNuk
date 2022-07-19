cf282a125995872d9295e55a5a51a72b36788312
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-19 19:46:18 +0900

    07/19

---
+++ CS_Study/CS_Study/2739.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _2739
+    {
+        private static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= 9; i++)
+            {
+                Console.Write("{0} * {1} = {2}", a, i, a * i);
+            }
+        }
+    }
+}
