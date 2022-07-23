b642e02e11e8d4a243a7c52f43b0d8e4a44992d3
GdeveloperNuk <88584189+GdeveloperNuk@users.noreply.github.com>
2022-07-23 09:56:15 +0900

    07/23

---
+++ CS_Study/CS_Study/2742.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Study
+{
+    class _2742
+    {
+        private static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = count; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
